dc92b36c6860a8a2c245956c27a4bca57d0122e1
Jaron <56154527+Jarondev@users.noreply.github.com>
2021-09-21 12:16:57 +0200

    FEAT: frmfase2 aandelen overview

--- Inlevingsspel/frmfase2.Designer.cs
+++ Inlevingsspel/frmfase2.Designer.cs
@@ -29,12 +29,92 @@ namespace Inlevingsspel
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(249, 16);
+            this.label1.TabIndex = 123;
+            this.label1.Text = "Fase 2: Toe eigenen van aandelen";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label2.Location = new System.Drawing.Point(13, 25);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(249, 35);
+            this.label2.TabIndex = 124;
+            this.label2.Text = "Land: ";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 73);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(181, 20);
+            this.label3.TabIndex = 125;
+            this.label3.Text = "Aandelen in Duitsland: 0";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 96);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(245, 39);
+            this.button1.TabIndex = 126;
+            this.button1.Text = "Grondstoffen opeisen";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 147);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(181, 20);
+            this.label4.TabIndex = 127;
+            this.label4.Text = "Aandelen in Duitsland: 0";
+            // 
+            // frmfase2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "frmfase2";
+            this.ClientSize = new System.Drawing.Size(278, 198);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.Name = "frmfase2";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Fase 2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
     }
 }
--- Inlevingsspel/frmfase2.cs
+++ Inlevingsspel/frmfase2.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Inlevingsspel.Landen;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,31 @@ namespace Inlevingsspel
 {
     public partial class frmfase2 : Form
     {
-        public frmfase2()
+        public frmfase2(Land land)
         {
             InitializeComponent();
+            label2.Text = "Land: " + land.Naam;
+            int aandelenCount = 0;
+            for(int i = 0; i < land.Aandelen.Length; i++)
+            {
+                if(land.Aandelen[i] != 0 && i != land.LandID)
+                {
+                    Label labelAandeelText = new Label()
+                    {
+                        Location = new Point(13, 73 + (74 * aandelenCount)),
+                        Text = "Aandelen in " + frmspel.Landen[i] + ": " + land.Aandelen[i]
+                    };
+                    Button btnLandAandeel = new Button()
+                    {
+                        Location = new Point(17, 100 + (74 * aandelenCount)),
+                        Size = new Size(245, 39),
+                        Text = "Grondstoffen opeisen",
+                        Name = i.ToString()
+                    };
+                    aandelenCount++;
+                }
+            }
+
         }
     }
 }
